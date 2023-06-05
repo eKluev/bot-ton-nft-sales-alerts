b8bc33fe7f36f7056c31a75715dd1a8ffcbf798f
Eugene Kluev <me@kluev.eu>
2023-06-05 20:50:41 +0700

    try-catch everywhere

--- save_data.py
+++ save_data.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import bugsnag
 from threading import Thread
 from core import connector as conn
 
@@ -20,132 +21,150 @@ class Daemon(Thread):
             time.sleep(self.delay)
 
     def save_data(self):
-        data = requests.get(f"https://tonapi.io/v1/blockchain/getTransactions?account={self.royalty_wallet}&minLt={self.get_max_lt()}")
+        try:
+            data = requests.get(f"https://tonapi.io/v1/blockchain/getTransactions?account={self.royalty_wallet}&minLt={self.get_max_lt()}")
 
-        if data.status_code != 200:
-            print('No 200 from tonapi.io')
-            return None
-        else:
-            data: dict = data.json()
-            
-            if not data['transactions']:
+            if data.status_code != 200:
+                print('No 200 from tonapi.io')
                 return None
+            else:
+                data: dict = data.json()
 
-        for transaction in reversed(data['transactions']):
-            if 'in_msg' in transaction and 'source' in transaction['in_msg'] and 'address' in transaction['in_msg']['source']:
-                if self.check_transaction_type(transaction['in_msg']['source']['address']) == 'nft_sale' or transaction['in_msg']['msg_data'] == 'AAAAAE9mZmVyIHJveWFsaWVz':
-                    result = self.get_nft_sale_data(transaction['in_msg']['source']['address'])
-                    
-                    if transaction['in_msg']['msg_data'] == 'AAAAAE9mZmVyIHJveWFsaWVz':
-                        result['nft_sale_type'] = 'auction'
+                if not data['transactions']:
+                    return None
+
+            for transaction in reversed(data['transactions']):
+                if 'in_msg' in transaction and 'source' in transaction['in_msg'] and 'address' in transaction['in_msg']['source']:
+                    if self.check_transaction_type(transaction['in_msg']['source']['address']) == 'nft_sale' or transaction['in_msg']['msg_data'] == 'AAAAAE9mZmVyIHJveWFsaWVz':
+                        result = self.get_nft_sale_data(transaction['in_msg']['source']['address'])
+
+                        if transaction['in_msg']['msg_data'] == 'AAAAAE9mZmVyIHJveWFsaWVz':
+                            result['nft_sale_type'] = 'auction'
+                        else:
+                            result['nft_sale_type'] = 'instant buy'
+
+                        result['utime'] = transaction['utime']
+                        result['lt'] = transaction['lt']
+
+                        q = f"INSERT IGNORE INTO sales(nft_address, name, marketplace, price, sale_type,  owner_address, attr_theme, attr_type, collection_address, collection_name, image, utime, ltime) " \
+                            f"VALUES ('{result['nft_address']}','{result['name']}','{result['marketplace']}',{result['price']},'{result['nft_sale_type']}','{result['owner_address']}','{result['theme']}','{result['type']}','{result['collection']['address']}','{result['collection']['name']}','{result['image']}',{result['utime']},{result['lt']})"
+                        conn.make_query(q, commit=True)
+                        time.sleep(15)
                     else:
-                        result['nft_sale_type'] = 'instant buy'
-                        
-                    result['utime'] = transaction['utime']
-                    result['lt'] = transaction['lt']
-                    
-                    q = f"INSERT IGNORE INTO sales(nft_address, name, marketplace, price, sale_type,  owner_address, attr_theme, attr_type, collection_address, collection_name, image, utime, ltime) " \
-                        f"VALUES ('{result['nft_address']}','{result['name']}','{result['marketplace']}',{result['price']},'{result['nft_sale_type']}','{result['owner_address']}','{result['theme']}','{result['type']}','{result['collection']['address']}','{result['collection']['name']}','{result['image']}',{result['utime']},{result['lt']})"
-                    conn.make_query(q, commit=True)
-                    time.sleep(15)
-                else:
-                    continue
+                        continue
+        except Exception as e:
+            bugsnag.notify(e)
 
     def check_transaction_type(self, address) -> str | bool:
-        data = requests.get(f"https://tonapi.io/v1/account/getInfo?account={address}")
+        try:
+            data = requests.get(f"https://tonapi.io/v1/account/getInfo?account={address}")
 
-        if data.status_code != 200:
-            time.sleep(5)
-            return self.check_transaction_type(address)
-        else:
-            data: dict = data.json()
-            
-        if 'interfaces' not in data or data['interfaces'] is None:
-            return False
-        elif 'nft_sale_get_gems' in data['interfaces'] or 'nft_sale' in data['interfaces']:
-            return 'nft_sale'
-        elif 'nft_item' in data['interfaces']:
-            return 'nft_item'
-        else:
-            return False
+            if data.status_code != 200:
+                time.sleep(5)
+                return self.check_transaction_type(address)
+            else:
+                data: dict = data.json()
+
+            if 'interfaces' not in data or data['interfaces'] is None:
+                return False
+            elif 'nft_sale_get_gems' in data['interfaces'] or 'nft_sale' in data['interfaces']:
+                return 'nft_sale'
+            elif 'nft_item' in data['interfaces']:
+                return 'nft_item'
+            else:
+                return False
+        except Exception as e:
+            bugsnag.notify(e)
 
     def get_nft_sale_data(self, nft_sale_address) -> dict:
-        data = requests.get(f"https://tonapi.io/v1/blockchain/getTransactions?account={nft_sale_address}")
+        try:
+            data = requests.get(f"https://tonapi.io/v1/blockchain/getTransactions?account={nft_sale_address}")
 
-        if data.status_code != 200:
-            time.sleep(5)
-            return self.get_nft_sale_data(nft_sale_address)
-        else:
-            data: dict = data.json()
-        
-        result = dict()
-        nft_address = None
+            if data.status_code != 200:
+                time.sleep(5)
+                return self.get_nft_sale_data(nft_sale_address)
+            else:
+                data: dict = data.json()
 
-        for transaction in data['transactions']:
-            if transaction['out_msgs']:
-                for t_out in transaction['out_msgs']:
-                    if self.check_transaction_type(t_out['destination']['address']) == 'nft_item':
-                        nft_address = t_out['destination']['address']
+            result = dict()
+            nft_address = None
 
-        result['nft_address'] = self.get_bounceable_address(nft_address)
-        result.update(self.get_nft_data(result['nft_address']))
+            for transaction in data['transactions']:
+                if transaction['out_msgs']:
+                    for t_out in transaction['out_msgs']:
+                        if self.check_transaction_type(t_out['destination']['address']) == 'nft_item':
+                            nft_address = t_out['destination']['address']
 
-        result['price'] = 0
-        for transaction in data['transactions']:
-            if 'source' in transaction['in_msg'] and transaction['in_msg']['source']['address'] == result['base_64_owner_address']:
-                price: float = round(float(int(transaction['in_msg']['value']) / 10 ** 9 - 1), 2)
-                if result['price'] < price:
-                    result['price'] = price
-            
-            if len(transaction['out_msgs']) == 4:
-                marketplace = 'Unknown'
-                for t in transaction['out_msgs']:
-                    if t['source']['address'] in self.marketplaces.values():
-                        marketplace = list(self.marketplaces.keys())[list(self.marketplaces.values()).index(t['source']['address'])]
-                    elif t['destination']['address'] in self.marketplaces.values():
-                        marketplace = list(self.marketplaces.keys())[list(self.marketplaces.values()).index(t['destination']['address'])]
+            result['nft_address'] = self.get_bounceable_address(nft_address)
+            result.update(self.get_nft_data(result['nft_address']))
 
-                    result['marketplace'] = marketplace
+            result['price'] = 0
+            for transaction in data['transactions']:
+                if 'source' in transaction['in_msg'] and transaction['in_msg']['source']['address'] == result['base_64_owner_address']:
+                    price: float = round(float(int(transaction['in_msg']['value']) / 10 ** 9 - 1), 2)
+                    if result['price'] < price:
+                        result['price'] = price
 
-        return result
+                if len(transaction['out_msgs']) == 4:
+                    marketplace = 'Unknown'
+                    for t in transaction['out_msgs']:
+                        if t['source']['address'] in self.marketplaces.values():
+                            marketplace = list(self.marketplaces.keys())[list(self.marketplaces.values()).index(t['source']['address'])]
+                        elif t['destination']['address'] in self.marketplaces.values():
+                            marketplace = list(self.marketplaces.keys())[list(self.marketplaces.values()).index(t['destination']['address'])]
+
+                        result['marketplace'] = marketplace
+
+            return result
+        except Exception as e:
+            bugsnag.notify(e)
 
     def get_nft_data(self, nft_address: str) -> dict:
-        data = requests.get(f"https://tonapi.io/v2/nfts/{nft_address}")
+        try:
+            data = requests.get(f"https://tonapi.io/v2/nfts/{nft_address}")
 
-        if data.status_code != 200:
-            time.sleep(5)
-            return self.get_nft_data(nft_address)
-        else:
-            data: dict = data.json()
-            
-        if len(data) == 0:
-            return self.get_nft_data(nft_address)
+            if data.status_code != 200:
+                time.sleep(5)
+                return self.get_nft_data(nft_address)
+            else:
+                data: dict = data.json()
 
-        data['collection']['address'] = self.get_bounceable_address(data['collection']['address'])
-        collection: dict = data['collection']
-        name: str = data['metadata']['name']
-        image: str = data['metadata']['image']
-        owner_address: str = data['owner']['address']
+            if len(data) == 0:
+                return self.get_nft_data(nft_address)
 
-        for attr in data['metadata']['attributes']:
-            if attr['trait_type'] == 'type':
-                attr_type: str = attr['value']
+            data['collection']['address'] = self.get_bounceable_address(data['collection']['address'])
+            collection: dict = data['collection']
+            name: str = data['metadata']['name']
+            image: str = data['metadata']['image']
+            owner_address: str = data['owner']['address']
 
-            if attr['trait_type'] == 'theme':
-                attr_theme: str = attr['value']
-                    
-        return {"collection": collection, "name": name, "image": image, "type": attr_type, "theme": attr_theme, "base_64_owner_address": owner_address, "owner_address": self.get_bounceable_address(owner_address)}
+            for attr in data['metadata']['attributes']:
+                if attr['trait_type'] == 'type':
+                    attr_type: str = attr['value']
+
+                if attr['trait_type'] == 'theme':
+                    attr_theme: str = attr['value']
+
+            return {"collection": collection, "name": name, "image": image, "type": attr_type, "theme": attr_theme, "base_64_owner_address": owner_address, "owner_address": self.get_bounceable_address(owner_address)}
+        except Exception as e:
+            bugsnag.notify(e)
 
     def get_bounceable_address(self, base64_address:str) -> str:
-        data = requests.get(f"https://tonapi.io/v1/account/getInfo?account={base64_address}")
+        try:
+            data = requests.get(f"https://tonapi.io/v1/account/getInfo?account={base64_address}")
 
-        if data.status_code != 200:
-            time.sleep(5)
-            return self.get_bounceable_address(base64_address)
-        else:
-            data: dict = data.json()
-            return data['address']['bounceable']
+            if data.status_code != 200:
+                time.sleep(5)
+                return self.get_bounceable_address(base64_address)
+            else:
+                data: dict = data.json()
+                return data['address']['bounceable']
+        except Exception as e:
+            bugsnag.notify(e)
             
     def get_max_lt(self):
-        q = "select max(ltime) as ltime from sales"
-        return int(conn.make_query(q)[0]['ltime'])
+        try:
+            q = "select max(ltime) as ltime from sales"
+            return int(conn.make_query(q)[0]['ltime'])
+        except Exception as e:
+            bugsnag.notify(e)
--- send_alert.py
+++ send_alert.py
@@ -17,37 +17,40 @@ class Daemon(Thread):
         chats = ["-1001634937384", "-1001774539187"]
         
         while True:
-            data = conn.make_query("select * from alerts as a left join sales as s on s.id = a.sales_id where a.notification_sent = 0")
-            ton_usd = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd")
-            if ton_usd.status_code != 200 or 'the-open-network' not in ton_usd.json():
-                bugsnag.notify(Exception(f"CoinGeko API failed with code {ton_usd.status_code}: {ton_usd.content}"))
-                return
+            try:
+                data = conn.make_query("select * from alerts as a left join sales as s on s.id = a.sales_id where a.notification_sent = 0")
+                ton_usd = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd")
+                if ton_usd.status_code != 200 or 'the-open-network' not in ton_usd.json():
+                    bugsnag.notify(Exception(f"CoinGeko API failed with code {ton_usd.status_code}: {ton_usd.content}"))
+                    return
 
-            ton_usd = ton_usd.json()['the-open-network']['usd']
-            for d in data:
-                if d['marketplace'] == "Getgems":
-                    message = f"<a href='https://getgems.io/nft/{d['nft_address']}'>{d['name']}</a>"
-                else:
-                    message = f"<a href='https://beta.disintar.io/object/{d['nft_address']}'>{d['name']}</a>"
-                    
-                message += "\n\n"
-                message += f"#{d['attr_type']}\n\n"
-                usd_price = round(ton_usd * float(d['price']), 2)
-                message += f"💎 Price: {d['price']} TON | $ {usd_price}\n\n"
-                message += f"🛍 Marketplace: #{d['marketplace']}\n\n"
-                message += f"🔖 Type of deal: {d['sale_type']}\n\n"
-                message += f"🕓 Date & time: {datetime.datetime.fromtimestamp(d['utime'])}\n"
+                ton_usd = ton_usd.json()['the-open-network']['usd']
+                for d in data:
+                    if d['marketplace'] == "Getgems":
+                        message = f"<a href='https://getgems.io/nft/{d['nft_address']}'>{d['name']}</a>"
+                    else:
+                        message = f"<a href='https://beta.disintar.io/object/{d['nft_address']}'>{d['name']}</a>"
 
-                for chat in chats:
-                    while True:
-                        data_msg = {"chat_id": chat, "photo": d['image'] + f'?a={time.time()}', "caption": message, "parse_mode": "HTML"}
-                        response = requests.post(url=f"https://api.telegram.org/bot{os.environ.get('telegram_token')}/sendPhoto", data=data_msg)
-                        if response.status_code == 200:
-                            conn.make_query(f"update alerts set notification_sent=1 where sales_id={d['id']}", commit=True)
-                            break
-                        else:
-                            print(response.json())
-                            time.sleep(10)
-                        time.sleep(2)
+                    message += "\n\n"
+                    message += f"#{d['attr_type']}\n\n"
+                    usd_price = round(ton_usd * float(d['price']), 2)
+                    message += f"💎 Price: {d['price']} TON | $ {usd_price}\n\n"
+                    message += f"🛍 Marketplace: #{d['marketplace']}\n\n"
+                    message += f"🔖 Type of deal: {d['sale_type']}\n\n"
+                    message += f"🕓 Date & time: {datetime.datetime.fromtimestamp(d['utime'])}\n"
+
+                    for chat in chats:
+                        while True:
+                            data_msg = {"chat_id": chat, "photo": d['image'] + f'?a={time.time()}', "caption": message, "parse_mode": "HTML"}
+                            response = requests.post(url=f"https://api.telegram.org/bot{os.environ.get('telegram_token')}/sendPhoto", data=data_msg)
+                            if response.status_code == 200:
+                                conn.make_query(f"update alerts set notification_sent=1 where sales_id={d['id']}", commit=True)
+                                break
+                            else:
+                                print(response.json())
+                                time.sleep(10)
+                            time.sleep(2)
+            except Exception as e:
+                bugsnag.notify(e)
 
             time.sleep(self.delay)
